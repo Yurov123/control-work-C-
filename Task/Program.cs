1ef6166d04091465a6ed83a99d94f173fe46d2dc
Konstantin <constantin8271@gmail.com>
2022-03-31 19:36:02 +0300

    task added

---
+++ Task/Program.cs
@@ -0,0 +1,15 @@
+﻿// Из имеющегося массива строк сформировать массив из строк,длинна которых меньше либо равна 3 символам.
+
+Console.Clear();
+
+string[] sourceArray = new string[] { "hello", "2", ":-)","work","list","Moscow","((","1"};
+string[] sortedArray = new string[sourceArray.Length];
+
+for (int i = 0; i < sourceArray.Length ; i++)
+{
+    if(sourceArray[i].Length <= 3)
+    {
+    sortedArray[i] = sourceArray[i];
+    Console.Write(sortedArray[i] + " ");
+    }
+}
